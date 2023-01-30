decf77b4876b66235d3f486d0ee9622c9ddf59c9
root <maxisy@maxisy.pl>
2023-01-30 21:54:50 +0100

    base UI + some logic

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>
+        {cartCtx.items.reduce((curNumber, item) => curNumber + item.amount, 0)}
+      </span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
