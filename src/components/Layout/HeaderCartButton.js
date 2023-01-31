464b53acb0c3d582a67f2ee74975d9d0dcb22b8b
root <maxisy@maxisy.pl>
2023-01-31 21:33:56 +0100

    complete app logic

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,31 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
+  const btnItems = `${classes.button} ${buttonIsHighlighted ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setButtonIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnItems} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
